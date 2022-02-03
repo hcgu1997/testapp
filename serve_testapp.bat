6c42f1b0816e631f80d67b656d069ca37ace5e77
hg134 <hg134@duke.edu>
2022-02-03 16:10:57 -0500

    ghc

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/JakeVestal/testapp.git
+cd C:\Users\hg134\Desktop\testapp
+git pull https://%TESTAPP_GIT_PAT%@github.com/hcgu1997/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
